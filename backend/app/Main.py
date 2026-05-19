06058db92c490a9dcdf8fab2058d5248862ad5f6
claudeayi <claudejordanayissi@gmail.com>
2026-05-19 19:32:45 +0100

    Update Main.py

--- backend/app/Main.py
+++ backend/app/Main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
+
 from app.core.config import settings
+
+from app.api.routes import leads
 
 app = FastAPI(
     title=settings.APP_NAME
+)
+
+app.include_router(
+    leads.router,
+    prefix="/leads",
+    tags=["Leads"]
 )
 
 @app.get("/")
